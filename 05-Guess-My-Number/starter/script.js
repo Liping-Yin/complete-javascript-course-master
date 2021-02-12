6278176434d9dd508520b4b84851f1f045b61201
Liping_Yin <liping.yin1221@gmail.com>
2021-02-12 23:32:41 +0100

    DOM manipulation
    writing two easy game about how to access elements, using class and event listener to manipulate document elements and their properties.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,111 @@
 'use strict';
+/*
+// ------------what is DOM and DOM manipulation
+// select and manipulate
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct number !';
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+// writing dry codes: no duplicate
+// when there are duplicated codes, refractor them into functions.
+
+//---------- Implementing the Guessing Game
+// Implement 'Check': handling click event
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// store some variable in the code, not just depend on HTML document.
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); // input is string !
+
+  // always check the input, truthy value;
+  // consider all possible situations
+
+  // when no input
+  if (!guess) {
+    displayMessage('No number entered 😂');
+    // document.querySelector('.message').textContent = ' No number entered 😂';
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!😘');
+    // document.querySelector('.message').textContent = 'Correct Number!😘';
+    // manipulate CSS style: all style value should be string;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = guess;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+
+  // refractor
+  // when guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      const message = guess > secretNumber ? '😂 Too high !' : '😂Too low!';
+      displayMessage(message);
+      //   document.querySelector('.message').textContent =
+      //     guess > secretNumber ? '😂 Too high !' : '😂Too low!';
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lose the game!😂');
+      //   document.querySelector('.message').textContent = ' You lose the game!😂';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  // below shows solutions with too many duplicate codes
+  // when guess too high
+  //    else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = ' Too high!😂';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = ' You lose the game!😂';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+
+  //     // when guess too low
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = ' Too low!😂';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = ' You lose the game!😂';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
+});
+
+// Implement Again button with event
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = null;
+  document.querySelector('.score').textContent = score;
+  displayMessage('start guessing...');
+  //   document.querySelector('.message').textContent = 'start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+// --------- focus on how to use class in HTML
+// aggregate different effects into different classes, instead of manipulating CSS style and change the value, just manipulate the class is easier.
+
+// be careful to use querySelectorAll and querySelector！！！
+// querySelector only show the satisfied first element
+// so instead use querySelectAll if want to select all elements.
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+// console.log(btnOpenModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener(
+    'click',
+    openModal
+    //    function () {
+    //     // console.log('button clicked');
+
+    //     // modal.style.display = 'block';
+    //     modal.classList.remove('hidden');
+    //     overlay.classList.remove('hidden');
+    //   }
+  );
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// event happening in keyboard: in the whole document
+// keydown, keyup, keypress
+// when event happens, information about the event can be retrieved
+document.addEventListener('keydown', function (event) {
+  //   console.log('a key is pressed');
+  //   console.log(event.key);
+
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+
+  //   if (event.key === 'Escape') {
+  //     if (!modal.classList.contains('hidden')) {
+  //       closeModal();
+  //     }
+  //   }
+});
