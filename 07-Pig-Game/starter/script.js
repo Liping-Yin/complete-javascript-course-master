afd2ce57d2b1f604cd7c829667913981d77b9002
Liping_Yin <liping.yin1221@gmail.com>
2021-02-14 01:03:35 +0100

    finish the game PIG
    improve the dry coding ;
    debug

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,4 +1,6 @@
 'use strict';
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const score0El = document.getElementById('score--0');
 // const score1 = document.querySelector('#score--0');
@@ -16,29 +18,81 @@ score1El.textContent = 0;
 // use class to control things
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  // update internal variables
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  // update visible part in web page
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active'); // if it is already active then it won't add again
+  player1El.classList.remove('player--active');
+};
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
+
+init();
 
 btnRoll.addEventListener('click', function () {
-  //1.generating random dice roll;
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  //2.display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  if (dice !== 1) {
-    // add dice to current score
-    currentScore += dice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-    // store scores to players in each round
-    scores[activePlayer] += currentScore;
-  } else {
-    // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+  // the basic condition is the game is not over
+  if (playing) {
+    //1.generating random dice roll;
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2.display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+      // change the background of active and inactive player
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+    }
   }
   //3. check for rolled 1: true, switch to next player
 });
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. add current score to player score
+    // store scores to players in each round
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. check if player score >= 100
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
