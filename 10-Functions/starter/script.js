7451541be09c98ec40b5374d7956ab6564a966e4
Liping_Yin <liping.yin1221@gmail.com>
2021-02-25 23:29:38 +0100

    more about function
    default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+// ============ default parameters ===============
+const bookings = [];
+const creatbooking = function (
+  flightNum,
+  numPassenger = 1,
+  price = 199 * numPassenger // when use this the parameter order matters
+) {
+  // ES5
+  //   numPassenger = numPassenger || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+creatbooking('LH123');
+creatbooking('Lk122', 2, 500); //overwrite default parameters
+creatbooking('Lk122', undefined, 500);
