63f86715b63debbe6655abf0704222e469d07c86
Liping_Yin <liping.yin1221@gmail.com>
2021-03-07 21:28:31 +0100

    Update script.js
    how to select, create insert and delete element

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,60 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+//===== select element =======
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+// by these two almost can finish the select tasks
+const header = document.querySelector('.header');
+console.log(header);
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+// when you know all the elements very well
+document.getElementById('section--1');
+document.getElementsByClassName('btn');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+// ======== Creating and Inserting elements ===========
+const message = document.createElement('div');
+message.classList.add('cookie-message'); // in the css file this class exist already
+message.textContent = 'we create an cookie element';
+message.innerHTML =
+  'we create an cookie element <button class = "btn btn-close-cookie">Got it </button>';
+
+// DOM element is unique,they only exist in one place at a time
+
+// # Insert
+// child node
+
+header.prepend(message); // add as first child
+header.append(message); // add as last child
+
+// by clone, an element exists mor ethan one place
+header.append(message.cloneNode(true));
+
+// siblings
+header.before(message);
+header.after(message);
+
+//# delete
+document
+  .querySelector('.btn-close-cookie')
+  .addEventListener('click', function () {
+    // message.parentElement.removeChild(message);
+    message.remove(); // won't work for message cloneNode
+  });
+
+//=============== Styles, Attribute and Classes =====
+
+//======= Modal window ======
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -18,8 +66,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
