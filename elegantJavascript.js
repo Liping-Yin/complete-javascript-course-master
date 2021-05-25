cb82db1407dedb5eaa72146480bc7fa2a22c4d4d
Liping_Yin <liping.yin1221@gmail.com>
2021-05-25 03:11:11 +0200

    elegant javascript
    functions

--- elegantJavascript.js
+++ elegantJavascript.js
@@ -1,3 +1,5 @@
+// -------------- LOOP ---------------
+
 /** Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
@@ -27,3 +29,46 @@ for (let y = 0; y < size; y++) {
 }
 
 console.log(board);
+
+// --------------------- Functions ------------------
+
+/** 
+//  return minimum number
+function min(num1, num2) {
+  return num1 <= num2 ? num1 : num2;
+}
+
+console.log(min(1, 10));
+
+// recursion
+function isEven(number) {
+  if ((number = 0)) {
+    return true;
+  } else if (number == 1) {
+    return false;
+  } else if (number < 0) {
+    return isEven(-number);
+  } else {
+    return isEven(number - 2);
+  }
+}
+console.log(isEven(2));
+
+// growing functions: count Bs
+function countChar(string, ch) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+*/
+
+// -----------Data Structures : Objects and Arrays --------
