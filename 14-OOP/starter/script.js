db42567195e4003dbad847aed61cd1aad898eb46
Liping_Yin <liping.yin1221@gmail.com>
2021-06-22 22:37:36 +0200

    Update script.js
    constructor function to create prototype, and inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,31 +5,50 @@
 // -------------------- Prototype creation ----------------
 // #1: constructor function
 const Person = function (firstName, age) {
+  // instance properties
   this.firstName = firstName;
   this.age = age;
   console.log(this);
+
+  // not good convention to add functions directly here
+  // this.dance = function () {
+  //   console.log('bear is dancing');
+  // };
 };
 
+// use new operator to call constructor function
+//1. new {} is Created
+//2.function is called, this = {}
+//3. {} linked to prototype
+//4.function automatically return {}
+const icylove = new Person('icylove', 3);
+console.log(icylove);
+console.log(icylove instanceof Person);
+
 //  ------ Prototypes inheritance -------------
+// prototypes
+
 //  creat methods for the prototype
 Person.prototype.calcAge = function () {
   console.log(this.age);
 };
 // create properties
-Person.prototype.species = 'Homo Sapiens';
+Person.prototype.species = 'Toy';
 
-//1. new () is Created
-//2.function is called, this = ()
-//3. () linked to prototype
-//4.function automatically return ()
-const lp = new Person('Lp', 18);
-lp.calcAge();
+// console.log(Person);
+console.log(Person.prototype);
+
 // try to understand all this relationship
-console.log(lp.__proto__);
-console.log(lp.__proto__ === Person.prototype); // true
+console.log(Object.getPrototypeOf(icylove));
+console.log(icylove.__proto__);
+console.log(icylove.__proto__ === Person.prototype); // true: this step is realized by the link
 console.log(Person.prototype.isPrototypeOf(Person)); //false
-console.log(Person.prototype.isPrototypeOf(lp)); // true
+console.log(Person.prototype.isPrototypeOf(icylove)); // true
 
 // check inheritance properties and methods
-console.log(lp.hasOwnProperty('firstName')); // true
-console.log(lp.hasOwnProperty('species')); // false, it is inheritance
+
+console.log(icylove.hasOwnProperty('firstName')); // true, in the constructor
+console.log(icylove.hasOwnProperty('species')); // false, it is inheritance
+
+// #2：ES6 Classes
+// #3: Object.create()
