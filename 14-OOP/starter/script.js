1665f9d2d7cc6e62d7ba658abd0ee8a1a98647c6
Liping_Yin <liping.yin1221@gmail.com>
2021-06-23 11:42:49 +0200

    Update script.js
    setter and getter;
    ES6 class declaration and expression;

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,9 +1,12 @@
 'use strict';
 // ***************ways to create prototype**********
+
+/*
 //Javascript doesn't have class definition like classic OOP
 
 // -------------------- Prototype creation ----------------
-// #1: constructor function
+//////////// #1: constructor function /////////////////////
+
 const Person = function (firstName, age) {
   // instance properties
   this.firstName = firstName;
@@ -53,5 +56,104 @@ console.log(Person.prototype.isPrototypeOf(icylove)); // true
 console.log(icylove.hasOwnProperty('firstName')); // true, in the constructor
 console.log(icylove.hasOwnProperty('species')); // false, it is inheritance
 
-// #2：ES6 Classes
-// #3: Object.create()
+
+/////////////////////////////////////////////
+
+// Coding challenge#1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const carBMW = new Car('BMW', 120);
+const carMercedes = new Car('Mercedes', 95);
+carBMW.accelerate();
+carBMW.accelerate();
+carBMW.brake();
+
+carMercedes.accelerate();
+carMercedes.brake();
+*/
+
+/////////////////// #2：ES6 Classes ////////////////////
+// 1: class expression
+//const PersonCl = class {};//class is a specail function
+// 2: class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // all properties written outside the construcotr will be in the prototype
+
+  dance() {
+    console.log('bear is dancing');
+  }
+}
+
+const icyloveCl = new PersonCl('icylove', 2019);
+console.log(icyloveCl);
+console.log(icyloveCl.hasOwnProperty('dance'));
+console.log(icyloveCl.hasOwnProperty('birthYear'));
+console.log(icyloveCl.__proto__ === PersonCl.prototype);
+
+// Attentions:
+/**
+ * 1.Classes are NOT hoisted, so have to declare first before use it
+ * 2.Class are firt-class citizens
+ * 3.Classes are executed in strict mode
+ */
+
+// every object can have its own getter and setter properties, not like a methods, when you call it
+const account = {
+  owner: 'icylove',
+  movements: [200, 250, 300, 100],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest); // get
+account.latest = 50; // set
+console.log(account.movements);
+
+class Toy {
+  // avoid confliction: set a property that already exists
+  // validation
+  constructor(fullName, age) {
+    this.fullName = fullName;
+    this.age = age;
+  }
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full fullName`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const cat = new Toy('icylove bear', 3);
+const fox = new Toy('fox', 4);
+
+// cat.fullName = 'icylove the cat';
+console.log(cat);
+console.log(fox);
+// console.log(cat.fullName);
+// console.log(cat.name);
+// console.log(cat._name);
+
+//////////////////// #3: Object.create() //////////////
